d9f864fec259901f9acbe8898458a7d208514c04
ThePhyrus <romaisgood@yandex.ru>
2022-07-17 18:23:26 +0300

    Подключил viewBinding и добавил fun setTextFields(string: String)

--- app/src/main/java/rom/bannikov/simplecalculator/MainActivity.kt
+++ app/src/main/java/rom/bannikov/simplecalculator/MainActivity.kt
@@ -2,15 +2,19 @@ package rom.bannikov.simplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import rom.bannikov.simplecalculator.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
-    fun setTextFields(string: String) {
-
+    fun setTextFields(string: String) = with(binding){
+        tvMathOperation.append(string)
     }
 }
